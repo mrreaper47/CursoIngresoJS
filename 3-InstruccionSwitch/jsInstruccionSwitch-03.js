1555f4f4089c40a82257122db76383df6245cccb
mrreaper47 <alv.ccna@gmail.com>
2021-07-20 22:08:02 -0300

    Correcciones Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -16,10 +16,10 @@ function mostrar()
 
 		switch (mesDelAño) {
 			case "Febrero":
-				alert("Este mes no tiene más de 29 días.")
+				alert("Este mes no tiene más de 29 días.");
 				break;
 			default:
-				alert("Este mes tiene 30 o más días")
+				alert("Este mes tiene 30 o más días");
 		}
 	
 	
--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -8,21 +8,21 @@ si tiene 31 días.
 ******************************************************************************/
 function mostrar()
 {
-	var mesDelAño;
+	var mesDelAno;
 
-	mesDelAño=document.getElementById("txtIdMes").value;
+	mesDelAno=document.getElementById("txtIdMes").value;
 
-		switch (mesDelAño) {
+		switch (mesDelAno) {
 			case "Abril":
 			case "Junio":
 			case "Septiembre":
 			case "Noviembre":
-				alert("Tiene 30 días.")
+				alert("Tiene 30 días.");
 				break;
 			case "Febrero":
-				alert("Tiene 28 días.")
+				alert("Tiene 28 días.");
 				break;
 			default:
-				alert("Tiene 31 días.")
+				alert("Tiene 31 días.");
 		}
 }//FIN DE LA FUNCIÓN
